f036aeb6eb6bb581f0a1b0da8aabbb3f34da7a4c
shkamb <shkamb@proton.me>
2026-04-10 15:21:29 -0700

    feat: add arg parsing and exit

--- shell.c
+++ shell.c
@@ -9,17 +9,34 @@ long sc(long n, long a1, long a2, long a3) {
 
 int main() {
     char b[128];
-    char *v[2] = {b, 0};
     while (1) {
         sc(0x2000004, 1, (long)"> ", 2);
         int n = sc(0x2000003, 0, (long)b, 128);
         if (n <= 1) continue;
         b[n-1] = 0;
+
+        if (b[0] == 'e' && b[1] == 'x' && b[2] == 'i' && b[3] == 't') {
+            sc(0x2000001, 0, 0, 0);
+        }
+
+        char *v[16];
+        int i = 0;
+        v[i++] = b;
+        
+        for (int j = 0; b[j] != 0; j++) {
+            if (b[j] == ' ') {
+                b[j] = 0;
+                v[i++] = &b[j+1];
+            }
+        }
+        v[i] = 0;
+
         if (sc(0x2000002, 0, 0, 0) == 0) {
-            sc(0x200003b, (long)b, (long)v, 0);
+            sc(0x200003b, (long)v[0], (long)v, 0);
             sc(0x2000001, 1, 0, 0);
         }
-        for(volatile int i=0; i<5000000; i++);
+        
+        for(volatile int k=0; k<1000000; k++);
     }
-return 0;
+    return 0;
 }
